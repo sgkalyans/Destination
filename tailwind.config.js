49d9222f72f6059c1b2b0a31e4e0950f9f5b85f1
sgkalyans <sgkalyans@gmail.com>
2025-08-05 18:59:09 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        turmeric: '#FFA500',
+        peacock: '#1E90FF',
+        maroon: '#B22222',
+        gold: '#FFD700',
+      }
+    }
+  }
+}
